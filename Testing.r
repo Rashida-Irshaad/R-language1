74efbacb1748e41bd6a13310b709f4b413d54687
Your Name <rashidaparveen9731@gmail.com>
2021-05-01 00:05:05 +0500

    one more line added.

--- Testing.r
+++ Testing.r
@@ -4,6 +4,7 @@ name ="sana"
 version ="r-studio in writing"
 print(name)
 print("these lines  has been written in r-studio")
-
+our_four_caliph ="ali R.A,osman R.A,umer R.A,abu bakar R.A"
+print("our_four_caliph")
 
 
